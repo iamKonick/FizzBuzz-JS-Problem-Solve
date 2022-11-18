2b30e886ed3d449482c4a8f4930d1177c0b19782
Md. Efthakhar Ul Alam <67385172+iamKonick@users.noreply.github.com>
2022-11-18 14:14:55 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+//function for printing Fizz/Buzz/FizzBuzz
+
+function fizzBuzzPrinter() {
+    for(let i=1;i<=100;i++){
+        if(i%3 === 0){
+            console.log(i + ' Fizz')
+        }
+        else if(i%5 === 0){
+            console.log( i + " Buzz")
+        }
+        else if(i%3 === 0 && i%5 === 0){
+            console.log(i + " FizzBuzz")
+        }
+
+        else{
+            console.log(i);
+        }
+    }
+    
+}
+
+fizzBuzzPrinter();
